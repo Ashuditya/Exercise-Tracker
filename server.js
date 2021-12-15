f86dc2374689802e6975118feb0e6691f2bb9d6c
Ashuditya <ashuditya@hotmail.com>
2021-12-15 12:30:31 +0530

    added post request for /api/users

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
 // const {response} = require('express');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 const exerciseSchema = mongoose.Schema({
   username: {type: String, required: true},
   description: String,
-  duration: {type: Number, required: true},
-  date: Date
+  duration: Number,
+  date: Date,
 });
 
-let Exercise = mongoose.model("Exercise", exerciseSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 const userSchema = mongoose.Schema({
-  username: {type: String, required: true},
+  username: {type: String},
 });
-let User = mongoose.Schema("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+app.post("/api/users", (req,res)=>{
+  console.log("req.body", req.body);
+  var username = req.body.username;
+  console.log(username);
+  const newUser = new User({
+    username: req.body.username
+  });
+  newUser.save((err,data)=>{
+    if(err || !data)
+      res.send("Error in saving the user");
+    else
+      res.json(data);
+  });
+
 });
 
 
