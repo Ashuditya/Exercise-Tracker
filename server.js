1eda3ff7f220da1bea4ab55fa392d3c6c9d5bac7
Ashuditya <ashuditya@hotmail.com>
2021-12-15 11:36:44 +0530

    added link to mongo db

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+// const {response} = require('express');
+mongoose.connect(process.env.MONGO_URI);
+
+const exerciseSchema = mongoose.Schema({
+  username: {type: String, required: true},
+  description: String,
+  duration: {type: Number, required: true},
+  date: Date
+});
+
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true},
+});
+let User = mongoose.Schema("User", userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
